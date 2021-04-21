9912fdb498432a7340f59b90c16c7ada1a4864ab
Daniil <danya_livitin@mail.ru>
2021-04-21 18:20:18 +0300

    Add work with linked hash map and tree map in the main class

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        /**
+        /*
          * Work with created singly linked list
          */
         SinglyLinkedList list = new SinglyLinkedList();
@@ -17,17 +19,48 @@ public class Main {
         list.remove(0);
         System.out.println(list);
 
-        /**
-         * Work with hashmap structure
+        /*
+         * Work with a hash map structure
+         * In hash map, the data is not structured. It means that it does not maintain the order of adding elements to the map.
+         * (When you output your map, your data can be outputted in a different order than the order of adding)
          */
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1,"one");
-        map.put(2,"two");
-        map.put(3,"three");
+        Map<String, String> map = new HashMap<>();
+        map.put("кошка","cat");
+        map.put("волк","wolf");
+        map.put("слон","elephant");
 
-        for(Map.Entry<Integer, String> entry: map.entrySet()){
+        for(Map.Entry<String , String> entry: map.entrySet()){
             System.out.println(entry.getKey()+" : " + entry.getValue());
         }
+
+
+        /*
+         * Work with a linked hash map structure
+         * In the linked hash map, the data is structured. It means that it maintains the order of adding elements to the map.
+         * (When you output your map, your data will be outputted in the order of adding)
+         */
+        Map<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("кошка","cat");
+        linkedHashMap.put("волк","wolf");
+        linkedHashMap.put("слон","elephant");
+
+        for(Map.Entry<String , String> entry: linkedHashMap.entrySet()){
+            System.out.println(entry.getKey()+" : " + entry.getValue());
+        }
+
+        /*
+         * Work with a tree map structure
+         * In the tree map, the data is sorted by key.
+         */
+        Map<String, String> treeMap = new TreeMap<>();
+        treeMap.put("кошка","cat");
+        treeMap.put("волк","wolf");
+        treeMap.put("слон","elephant");
+
+        for(Map.Entry<String , String> entry: treeMap.entrySet()){
+            System.out.println(entry.getKey()+" : " + entry.getValue());
+        }
+
 
     }
 }
