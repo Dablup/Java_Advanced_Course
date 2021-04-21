52f5f2f4f076140d3811e098ef080500d5d06442
Daniil <danya_livitin@mail.ru>
2021-04-21 18:03:17 +0300

    Add work with hashmap in the main class

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Work with created singly linked list
+         */
         SinglyLinkedList list = new SinglyLinkedList();
         list.add(1);
         list.add(0);
@@ -10,5 +16,18 @@ public class Main {
         list.remove(1);
         list.remove(0);
         System.out.println(list);
+
+        /**
+         * Work with hashmap structure
+         */
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1,"one");
+        map.put(2,"two");
+        map.put(3,"three");
+
+        for(Map.Entry<Integer, String> entry: map.entrySet()){
+            System.out.println(entry.getKey()+" : " + entry.getValue());
+        }
+
     }
 }
